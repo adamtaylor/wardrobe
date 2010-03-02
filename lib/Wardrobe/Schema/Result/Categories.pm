9da15565498d0fc47cdbf05c5530711c5e4b877d
adamtaylor <adamjctaylor@gmail.com>
2010-03-02 12:43:31 +0000

    commit all other files

--- lib/Wardrobe/Schema/Result/Categories.pm
+++ lib/Wardrobe/Schema/Result/Categories.pm
@@ -33,7 +33,22 @@ __PACKAGE__->has_many(
 
 # Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-03-02 10:58:05
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:smGSfqvqmRZFMDi3pxlvfA
+__PACKAGE__->many_to_many('clothes', 'category_clothes', 'clothing');
 
+sub check_and_create {
+    my ($self, $category) = @_;
+
+    my @categories = $self->search({ name => $category });
+    
+    my $size = scalar @categories;
+    
+    if ($size >= 1) {
+        return $categories[0];
+    }
+    
+    return $self;
+    
+}
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
--- lib/Wardrobe/Schema/Result/Outfits.pm
+++ lib/Wardrobe/Schema/Result/Outfits.pm
@@ -33,7 +33,7 @@ __PACKAGE__->has_many(
 
 # Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-03-02 10:58:05
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:pHw4CUHqti3oNN7T5Rmfog
-
+__PACKAGE__->many_to_many('clothes', 'outfit_clothes', 'clothing');
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
