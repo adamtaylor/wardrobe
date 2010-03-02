953d83dfdfa7b8c480c7a54a41c43d7505357c54
adamtaylor <adamjctaylor@gmail.com>
2010-03-02 10:54:38 +0000

    created sql schema for modesl

---
+++ db/db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    name TEXT
+);
+CREATE TABLE clothes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    name TEXT
+);
+CREATE TABLE outfits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    name TEXT
+);
+CREATE TABLE category_clothes (
+    category_id INTEGER REFERENCES categories(id),
+    clothing_id INTEGER REFERENCES clothes(id)
+    PRIMARY KEY( category_id, clothing_id )
+);
+CREATE TABLE outfit_clothes (
+    outfit_id INTEGER REFERENCES outfits(id),
+    clothing_id INTEGER REFERENCES clothes(id),
+    PRIMARY KEY( outfit_id, clothing_id );
+);
